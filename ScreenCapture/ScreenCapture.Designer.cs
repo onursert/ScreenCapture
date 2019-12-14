daae98b8a7ab1ae0dba5d0e3945e93b953f1bd6a
Onur <43381986+onursert@users.noreply.github.com>
2019-12-14 20:46:11 +0300

    Add files via upload

---
+++ ScreenCapture/ScreenCapture.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace ScreenCapture
+{
+    partial class ScreenCapture
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonStartStop = new System.Windows.Forms.Button();
+            this.buttonPauseResume = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonStartStop
+            // 
+            this.buttonStartStop.Location = new System.Drawing.Point(12, 12);
+            this.buttonStartStop.Name = "buttonStartStop";
+            this.buttonStartStop.Size = new System.Drawing.Size(75, 23);
+            this.buttonStartStop.TabIndex = 0;
+            this.buttonStartStop.Text = "Start";
+            this.buttonStartStop.UseVisualStyleBackColor = true;
+            this.buttonStartStop.Click += new System.EventHandler(this.buttonStartStop_Click);
+            // 
+            // buttonPauseResume
+            // 
+            this.buttonPauseResume.Enabled = false;
+            this.buttonPauseResume.Location = new System.Drawing.Point(12, 41);
+            this.buttonPauseResume.Name = "buttonPauseResume";
+            this.buttonPauseResume.Size = new System.Drawing.Size(75, 23);
+            this.buttonPauseResume.TabIndex = 1;
+            this.buttonPauseResume.Text = "Pause";
+            this.buttonPauseResume.UseVisualStyleBackColor = true;
+            this.buttonPauseResume.Click += new System.EventHandler(this.buttonPauseResume_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(93, 12);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(150, 23);
+            this.progressBar1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(249, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(230, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "github.com/onursert";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(344, 76);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.buttonPauseResume);
+            this.Controls.Add(this.buttonStartStop);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Screen Capture";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonStartStop;
+        private System.Windows.Forms.Button buttonPauseResume;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ ScreenCapture/ScreenCapture.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+using Microsoft.Expression.Encoder;
+using Microsoft.Expression.Encoder.Devices;
+using Microsoft.Expression.Encoder.ScreenCapture;
+using System.Collections.ObjectModel;
+
+namespace ScreenCapture
+{
+    public partial class ScreenCapture : Form
+    {
+        public ScreenCapture()
+        {
+            InitializeComponent();
+        }
+
+        private ScreenCaptureJob job = new ScreenCaptureJob();
+        private Job j = new Job();
+
+        private void buttonStartStop_Click(object sender, EventArgs e)
+        {
+            if (job.Status == RecordStatus.Running || job.Status == RecordStatus.Paused)
+            {
+                buttonPauseResume.Enabled = false;
+                buttonPauseResume.Text = "Pause";
+
+                job.Stop();
+                buttonStartStop.Text = "Saving...";
+                if (job.Status != RecordStatus.Running)
+                {
+                    Encode();
+                }
+                buttonStartStop.Text = "Start";
+            }
+            else
+            {
+                StartRecording();
+            }
+        }
+
+        private void StartRecording()
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                buttonPauseResume.Enabled = true;
+                progressBar1.Value = 0;
+                label1.Text = " ";
+
+                buttonStartStop.Text = "Stop";
+
+
+                job = new ScreenCaptureJob();
+
+                Size workingArea = SystemInformation.WorkingArea.Size;
+                //Size workingArea = SystemInformation.PrimaryMonitorSize; //Full Screen
+                Rectangle captureRect = new Rectangle(0, 0, workingArea.Width - (workingArea.Width % 4), workingArea.Height - (workingArea.Height % 4));
+                job.CaptureRectangle = captureRect;
+
+                //Properties
+                job.ScreenCaptureVideoProfile.FrameRate = 60;
+                job.ScreenCaptureVideoProfile.Quality = 100;
+                job.ScreenCaptureVideoProfile.SmoothStreaming = true;
+                job.CaptureMouseCursor = true;
+                job.ShowCountdown = true;
+                job.ShowFlashingBoundary = true;
+
+                job.AddAudioDeviceSource(AudioDevices());
+
+                job.OutputPath = folderBrowserDialog1.SelectedPath + "\\";
+                job.OutputScreenCaptureFileName = job.OutputPath + "ScreenCapture_" + DateTime.Today.ToString("MM-dd-yyyy ") + DateTime.Now.ToString("h.mm.ss tt") + ".xesc";
+
+                job.Start();
+            }
+        }
+
+        private EncoderDevice AudioDevices()
+        {
+            EncoderDevice foundDevice = null;
+            Collection<EncoderDevice> audioDevices = EncoderDevices.FindDevices(EncoderDeviceType.Audio);
+            try
+            {
+                foundDevice = audioDevices.FirstOrDefault();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("No audio device is found" + audioDevices[0].Name + ex.Message);
+            }
+            return foundDevice;
+        }
+
+        void Encode()
+        {
+            using (j = new Job())
+            {
+                MediaItem mediaItem = new MediaItem(job.OutputScreenCaptureFileName);
+                var size = mediaItem.OriginalVideoSize;
+                WindowsMediaOutputFormat format = new WindowsMediaOutputFormat();
+                format.VideoProfile = new Microsoft.Expression.Encoder.Profiles.AdvancedVC1VideoProfile();
+                format.AudioProfile = new Microsoft.Expression.Encoder.Profiles.WmaAudioProfile();
+                format.VideoProfile.AspectRatio = new System.Windows.Size(16, 9);
+                format.VideoProfile.AutoFit = true;
+                if (size.Width > 1920 && size.Height > 1080)
+                {
+                    format.VideoProfile.Size = new Size(1920, 1080);
+                    format.VideoProfile.Bitrate = new Microsoft.Expression.Encoder.Profiles.VariableUnconstrainedBitrate(6000);
+                }
+                else if (size.Width > 1280 && size.Height > 720)
+                {
+                    format.VideoProfile.Size = new Size(1280, 720);
+                    format.VideoProfile.Bitrate = new Microsoft.Expression.Encoder.Profiles.VariableUnconstrainedBitrate(4000);
+                }
+                else
+                {
+                    format.VideoProfile.Size = new Size(size.Width, Size.Height);
+                    format.VideoProfile.Bitrate = new Microsoft.Expression.Encoder.Profiles.VariableUnconstrainedBitrate(2000);
+                }
+
+                mediaItem.VideoResizeMode = VideoResizeMode.Letterbox;
+                mediaItem.OutputFormat = format;
+                j.MediaItems.Add(mediaItem);
+                j.CreateSubfolder = false;
+                j.OutputDirectory = folderBrowserDialog1.SelectedPath;
+                j.EncodeProgress += new EventHandler<EncodeProgressEventArgs>(j_encodeProgress);
+                j.Encode();
+            }
+        }
+
+        private void j_encodeProgress(object sender, EncodeProgressEventArgs e)
+        {
+            string status = string.Format("{0:F1}%", e.Progress);
+            label1.Text = "Step: " + e.CurrentPass + "/2 " + status;
+            progressBar1.Value = Convert.ToInt16(e.Progress);
+            Refresh();
+        }
+
+        private void buttonPauseResume_Click(object sender, EventArgs e)
+        {
+            if (job.Status == RecordStatus.Paused)
+            {
+                job.Resume();
+                buttonPauseResume.Text = "Pause";
+            }
+            else
+            {
+                job.Pause();
+                buttonPauseResume.Text = "Resume";
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (job != null)
+            {
+                if (job.Status == RecordStatus.Running)
+                {
+                    job.Stop();
+                }
+                job.Dispose();
+            }
+        }
+    }
+}
